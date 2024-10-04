cf9c37691dfe31ad680a25b8cb6e021569b4892b
Kelompok1-2A <yantibii5@gmail.com>
2024-10-04 21:59:02 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+
+
+
+let musicPlayer = document.querySelector(".music-container");
+let togglePlayer = document.querySelector(".toggle-player");
+
+let trackInfo = document.querySelector(".track-info");
+let trackName = document.querySelector(".trackname");
+let trackArtist = document.querySelector(".trackartist");
+let trackNav = document.querySelector(".track-nav");
+
+let playPauseBtn = document.querySelector(".playpause-track");
+let nextBtn = document.querySelector(".next-track");
+let prevBtn = document.querySelector(".prev-track");
+
+let trackIndex = 0;
+let isPlaying = false;
+let isHidden = true;
+
+let currentTrack = document.createElement("audio");
+let soundBars = document.querySelector(".sound-bars");
+
+togglePlayer.addEventListener("click", function() {
+    isHidden = !isHidden;
+    if(isHidden){
+        musicPlayer.classList.remove("hide");
+        togglePlayer.innerHTML = '<img class="w-[98%]" src="close.svg">';
+        trackInfo.style.transitionDelay = "0.4s";
+        trackNav.style.transitionDelay = "0.4s";
+    } else {
+        musicPlayer.classList.add("hide");
+        togglePlayer.innerHTML = '<img class="w-full" src="plus.svg">';
+        trackInfo.style.transitionDelay = "0s";
+        trackNav.style.transitionDelay = "0s";
+    }
+});
+
+let soundBarsLottie = bodymovin.loadAnimation({
+    container: soundBars,
+    renderer: "svg",
+    loop: true,
+    autoPLay: false,
+    path: "https://lottie.host/9ec12a7e-e429-453a-9f22-a2af1dcb4dca/2zeuy4rwtP.json",
+});
+
+
+let trackList = [
+ 
+    {
+        name: "Die With A Smile",
+        artist: "Lady Gaga & Bruno Mars",
+        path: "Die With A Smile.mp3",
+    },
+    
+    {
+        name: "The Only One",
+        artist: "Reyne",
+        path: "The Only One.mp3",
+    },
+];
+
+// EVENT LISTENERS
+playPauseBtn.addEventListener("click", playPauseTrack);
+nextBtn.addEventListener("click", nextTrack);
+prevBtn.addEventListener("click", prevTrack);
+
+function loadTrack(trackIndex){
+    currentTrack.src = trackList[trackIndex].path;
+    trackName.textContent = trackList[trackIndex].name;
+    trackArtist.textContent = trackList[trackIndex].artist;
+    currentTrack.addEventListener("ended", nextTrack);
+    currentTrack.load();
+}
+
+loadTrack(trackIndex);
+
+function playPauseTrack(){
+    if(isPlaying == false){
+        playTrack();
+    }else{
+        pauseTrack();
+    }
+}
+
+function playTrack(){
+    currentTrack.play();
+    isPlaying = true;
+    playPauseBtn.innerHTML = '<img class="w-8" src="pause.svg">';
+    soundBarsLottie.play();
+}
+
+function pauseTrack(){
+    currentTrack.pause();
+    isPlaying = false;
+    playPauseBtn.innerHTML = '<img class="w-8" src="play.svg">';
+    soundBarsLottie.stop();
+}
+
+function nextTrack(){
+    if(trackIndex < trackList.length - 1){
+        trackIndex += 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = 0;
+        loadTrack(trackIndex);
+        playTrack();
+    } 
+}
+
+function prevTrack(){
+    if(trackIndex > 0){
+        trackIndex -= 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }else{
+        trackIndex = trackList.length - 1;
+        loadTrack(trackIndex);
+        playTrack();
+    }
+}
